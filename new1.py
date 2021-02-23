833d35a0b3d9ab9f1ac414a7f960c7b7e0ea0f2a
eshita <isha7800@gmail.com>
2021-02-23 21:03:25 +0530

    write small comment

---
+++ new1.py
@@ -0,0 +1,17 @@
+git status:
+    shows the status of files (tracked/untracked)
+git commit -m "commit message could be anything"
+git add .
+# adds all files of folder to git
+git add new.py
+# will add only new.py
+git push origin master
+# will push to master branch in git
+
+# enter username and password when asked
+
+# follow these steps
+# copy your files in this python folder then 
+# type ls to see if they are added 
+# git status will also help you 
+# you can do this one or two times also see a tutorial 
